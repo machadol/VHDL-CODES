7b2903785c0f88527ae73aa0b9e04a80b25ec185
machadol <lucasmmsilva@gmail.com>
2017-09-07 11:22:12 -0300

    Laboratório 1 - Exercício 2

--- lab1_clk_div/lab1_clk_div.runs/impl_1/top_module.tcl
+++ lab1_clk_div/lab1_clk_div.runs/impl_1/top_module.tcl
@@ -43,23 +43,88 @@ proc step_failed { step } {
 }
 
 
-start_step write_bitstream
-set ACTIVE_STEP write_bitstream
+start_step init_design
+set ACTIVE_STEP init_design
 set rc [catch {
-  create_msg_db write_bitstream.pb
-  open_checkpoint top_module_routed.dcp
+  create_msg_db init_design.pb
+  create_project -in_memory -part xc7a35tcpg236-1
+  set_property board_part digilentinc.com:basys3:part0:1.1 [current_project]
+  set_property design_mode GateLvl [current_fileset]
+  set_param project.singleFileAddWarning.threshold 0
   set_property webtalk.parent_dir /home/lucas/Documents/UnB/Disciplinas/ED2/VHDL-CODES/lab1_clk_div/lab1_clk_div.cache/wt [current_project]
-  catch { write_mem_info -force top_module.mmi }
-  write_bitstream -force top_module.bit -bin_file
-  catch {write_debug_probes -no_partial_ltxfile -quiet -force debug_nets}
-  catch {file copy -force debug_nets.ltx top_module.ltx}
-  close_msg_db -file write_bitstream.pb
+  set_property parent.project_path /home/lucas/Documents/UnB/Disciplinas/ED2/VHDL-CODES/lab1_clk_div/lab1_clk_div.xpr [current_project]
+  set_property ip_output_repo /home/lucas/Documents/UnB/Disciplinas/ED2/VHDL-CODES/lab1_clk_div/lab1_clk_div.cache/ip [current_project]
+  set_property ip_cache_permissions {read write} [current_project]
+  add_files -quiet /home/lucas/Documents/UnB/Disciplinas/ED2/VHDL-CODES/lab1_clk_div/lab1_clk_div.runs/synth_1/top_module.dcp
+  read_xdc /home/lucas/Documents/UnB/Disciplinas/ED2/VHDL-CODES/lab1_clk_div/lab1_clk_div.srcs/constrs_1/imports/lab1_clk_div/Basys3_Master.xdc
+  link_design -top top_module -part xc7a35tcpg236-1
+  close_msg_db -file init_design.pb
 } RESULT]
 if {$rc} {
-  step_failed write_bitstream
+  step_failed init_design
   return -code error $RESULT
 } else {
-  end_step write_bitstream
+  end_step init_design
+  unset ACTIVE_STEP 
+}
+
+start_step opt_design
+set ACTIVE_STEP opt_design
+set rc [catch {
+  create_msg_db opt_design.pb
+  opt_design 
+  write_checkpoint -force top_module_opt.dcp
+  catch { report_drc -file top_module_drc_opted.rpt }
+  close_msg_db -file opt_design.pb
+} RESULT]
+if {$rc} {
+  step_failed opt_design
+  return -code error $RESULT
+} else {
+  end_step opt_design
+  unset ACTIVE_STEP 
+}
+
+start_step place_design
+set ACTIVE_STEP place_design
+set rc [catch {
+  create_msg_db place_design.pb
+  implement_debug_core 
+  place_design 
+  write_checkpoint -force top_module_placed.dcp
+  catch { report_io -file top_module_io_placed.rpt }
+  catch { report_utilization -file top_module_utilization_placed.rpt -pb top_module_utilization_placed.pb }
+  catch { report_control_sets -verbose -file top_module_control_sets_placed.rpt }
+  close_msg_db -file place_design.pb
+} RESULT]
+if {$rc} {
+  step_failed place_design
+  return -code error $RESULT
+} else {
+  end_step place_design
+  unset ACTIVE_STEP 
+}
+
+start_step route_design
+set ACTIVE_STEP route_design
+set rc [catch {
+  create_msg_db route_design.pb
+  route_design 
+  write_checkpoint -force top_module_routed.dcp
+  catch { report_drc -file top_module_drc_routed.rpt -pb top_module_drc_routed.pb -rpx top_module_drc_routed.rpx }
+  catch { report_methodology -file top_module_methodology_drc_routed.rpt -rpx top_module_methodology_drc_routed.rpx }
+  catch { report_power -file top_module_power_routed.rpt -pb top_module_power_summary_routed.pb -rpx top_module_power_routed.rpx }
+  catch { report_route_status -file top_module_route_status.rpt -pb top_module_route_status.pb }
+  catch { report_clock_utilization -file top_module_clock_utilization_routed.rpt }
+  catch { report_timing_summary -warn_on_violation -max_paths 10 -file top_module_timing_summary_routed.rpt -rpx top_module_timing_summary_routed.rpx }
+  close_msg_db -file route_design.pb
+} RESULT]
+if {$rc} {
+  write_checkpoint -force top_module_routed_error.dcp
+  step_failed route_design
+  return -code error $RESULT
+} else {
+  end_step route_design
   unset ACTIVE_STEP 
 }
 

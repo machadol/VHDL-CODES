4be3d3147d2ed085547be9ae4f28c2e31d9425f6
machadol <lucasmmsilva@gmail.com>
2017-09-13 22:01:20 -0300

    Prática de Eletrônica Digital 2

---
+++ Lab1_DIV_CLK/Lab1_DIV_CLK.srcs/sources_1/new/div_clk.vhd
@@ -0,0 +1,38 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity div_clk is
+	Port (  
+	       reset  :  in STD_LOGIC;
+		   clk    :  in STD_LOGIC;
+		   outclk : out STD_LOGIC;
+		   preset :  in STD_LOGIC_VECTOR(26 downto 0));
+end div_clk;
+
+architecture Behavioral of div_clk is
+
+	signal count  : STD_LOGIC_VECTOR(26 downto 0) := (others=>'0');
+	signal clkaux : STD_LOGIC := '0';
+
+begin
+
+process(clk, reset)
+begin
+    if rising_edge(clk) then
+	   if reset='1' then
+	       count <= (others=>'0');
+		   clkaux <= '0';
+	   elsif count=preset then
+	       clkaux <= not clkaux;
+	       count <= (others=>'0');
+	   else
+	       count <= count + '1';
+	   end if;
+    end if;
+end process;
+
+outclk <= clkaux;
+
+end Behavioral;
